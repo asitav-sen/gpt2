67cffb4519da4e76cb3d3d6c07b3eddb3ae469e0
asitav-sen <asitav.sen@gmail.com>
2021-06-19 00:25:46 +0530

    added dospace in board

--- R/download.R
+++ R/download.R
@@ -11,12 +11,12 @@
 #' pins prefixed with \code{gpt2_}.
 #'
 #' @export
-gpt2_download <- function(model = c("124M", "355M", "774M", "1558M")) {
+gpt2_download <- function(model = c("124M", "355M", "774M", "1558M"), board="dospace") {
   model <- match.arg(model, c("124M", "355M", "774M", "1558M"))
 
   model_base <- paste0("https://openaipublic.blob.core.windows.net/gpt-2/models/", model, "/")
   model_files <- c("checkpoint", "encoder.json", "hparams.json", "model.ckpt.data-00000-of-00001", "model.ckpt.index", "model.ckpt.meta", "vocab.bpe")
   model_urls <- paste0(model_base, model_files)
 
-  pins::pin(model_urls, name = paste("gpt2", model, sep = "_"), board = "local")
+  pins::pin(model_urls, name = paste("gpt2", model, sep = "_"), board = board)
 }
--- R/model.R
+++ R/model.R
@@ -5,17 +5,18 @@ gpt2_run <- function(prompt = "Hello my name is",
                      total_tokens = NULL,
                      temperature = 1,
                      top_k = 0,
-                     top_p = 1) {
+                     top_p = 1,
+                     board="dospace") {
   model <- match.arg(model, choices = c("124M", "355M", "774M", "1558M"))
   install_gpt2_verify()
 
   pin_name <- paste("gpt2", model, sep = "_")
-  if (nrow(pins::pin_find(name = pin_name, board = "local")) == 0) gpt2_download(model = model)
+  if (nrow(pins::pin_find(name = pin_name, board = board)) == 0) gpt2_download(model = model)
 
   py_path <- system.file("python", package = "gpt2")
   py_gpt2 <- reticulate::import_from_path("gpt2", path = py_path)
 
-  model_path <- dirname(dirname(pins::pin_get(name = pin_name, board = "local")[1]))
+  model_path <- dirname(dirname(pins::pin_get(name = pin_name, board = board)[1]))
   encoder <- py_gpt2$encoder$get_encoder(pin_name, model_path)
   gpt2 <- py_gpt2$gpt2
 
